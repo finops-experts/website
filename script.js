3b3d82193429a19c03a15dbef1fce73c0475c343
Williams <nwachukwuwilliams@outlook.com>
2024-12-17 01:16:54 +0100

    Update

--- script.js
+++ script.js
@@ -35,3 +35,29 @@ function tabOperation() {
     operation.classList.add('active');
     optimization.classList.remove('active');
 }
+
+
+
+const boxes = document.getElementsByClassName('drop');
+
+for (const box of boxes) {
+    box.addEventListener('click', function handleClick() {
+        box.classList.toggle('active');
+    });
+}
+
+
+const intersectionCallback = (entries) => {
+    for (const entry of entries) {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('fadeInUp');
+        }
+    }
+}
+
+const observer = new IntersectionObserver(intersectionCallback);
+
+const items = document.querySelectorAll('.wow');
+for (const item of items) {
+    observer.observe(item);
+}
